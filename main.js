5d14ee83032c6fffd6fb8e7f2215dfbd0619d9fc
Abdullah Turky <abdullahturki087@gmail.com>
2021-08-30 20:16:19 +0600

    updated

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-// window.addEventListener("keydown", function(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     if(!audio) return;
-//     audio.play();
-//     console.log(audio)
-// });
+function playSound(e) {
+    
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return; //stop the function to run all together
+
+    audio.currentTime = 0; //play from the start
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip if it's not transformed
+    e.target.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
